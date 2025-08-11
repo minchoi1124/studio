a7cffaa05dc07da69616ac0a6895f4f77f43d3d8
Min Hyuk Choi <minhc3@uci.edu>
2025-08-11 14:37:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/rich-text-editor.tsx
+++ src/components/rich-text-editor.tsx
@@ -1,45 +1,51 @@
 "use client";
-import React from 'react';
-import ReactQuill from 'react-quill';
+import React, { forwardRef } from 'react';
+import ReactQuill, { ReactQuillProps } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-interface RichTextEditorProps {
+interface RichTextEditorProps extends ReactQuillProps {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
 }
 
-const RichTextEditor = ({ value, onChange, placeholder }: RichTextEditorProps) => {
-  const modules = {
-    toolbar: [
-      [{ 'header': [1, 2, 3, false] }],
-      ['bold', 'italic', 'underline', 'strike'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link'],
-      ['clean']
-    ],
-  };
+const RichTextEditor = forwardRef<ReactQuill, RichTextEditorProps>(
+  ({ value, onChange, placeholder, ...props }, ref) => {
+    const modules = {
+      toolbar: [
+        [{ 'header': [1, 2, 3, false] }],
+        ['bold', 'italic', 'underline', 'strike'],
+        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+        ['link'],
+        ['clean']
+      ],
+    };
 
-  const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike',
-    'list', 'bullet', 'indent',
-    'link'
-  ];
+    const formats = [
+      'header',
+      'bold', 'italic', 'underline', 'strike',
+      'list', 'bullet', 'indent',
+      'link'
+    ];
 
-  return (
-    <div className="bg-background">
-      <ReactQuill
-        theme="snow"
-        value={value}
-        onChange={onChange}
-        modules={modules}
-        formats={formats}
-        placeholder={placeholder}
-        className="bg-white"
-      />
-    </div>
-  );
-};
+    return (
+      <div className="bg-background">
+        <ReactQuill
+          ref={ref}
+          theme="snow"
+          value={value}
+          onChange={onChange}
+          modules={modules}
+          formats={formats}
+          placeholder={placeholder}
+          className="bg-white"
+          {...props}
+        />
+      </div>
+    );
+  }
+);
+
+RichTextEditor.displayName = 'RichTextEditor';
 
 export default RichTextEditor;
